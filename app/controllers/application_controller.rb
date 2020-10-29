c83dccabae449d66a8c092ea90e904d8a3d99290
upgradeseason <junglerainforest@gmail.com>
2020-10-29 11:26:02 +0700

    Add welcome message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def welcome
+    render html: "Welcome to the blogging and content sharing platform dedicated to upgrading our lives and mindset!"
+  end
 end
