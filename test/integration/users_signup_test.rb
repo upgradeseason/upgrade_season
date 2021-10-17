8d69f1781a7fa357fa7a24ad7c14ebd1cf83154c
upgradeseason <junglerainforest@gmail.com>
2021-10-17 15:32:21 -0700

    Update invalid signup test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,26 +6,31 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # end
 
   test "invalid signup information" do
-    get signup_path #Visit signup path to doublecheck signup form renders without error
-    #Issue post request to users_path (by using the post function)
-    assert_no_difference 'User.count' do #User.count is a string argument to #assert_no_diff... method
-      post users_path, params: { user: { name:  "", #params[:user] hash is expected by the User.new create action.
-                                        email: "user@invalid",
-                                        password:              "foo",
-                                        password_confirmation: "bar" }}
+    get signup_path #We have a named path, we visit it. 
+    #Simulate going to signup page to catch bad mistakes/major regressions, aka verify renders w/o error.
+    #However we do issue POST request to users_path(the way submit button does) so signup_path isn't necessary.
+    assert_no_difference 'User.count' do #User.count is a string argument to #assert_no_difference method. 
+      #The count method is avail on every ActiveRecord class.
+      post users_path, params: { user: { name:  "", 
+      #POST request to users_path
+      #params[:user] hash is expected by the User.new create action.
+      #We're simulating form submission here.
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" }}
     end
-  assert_template 'users/new'
-  assert_select 'div#error_explanation'# hash in CSS targets a single specific element with unique ID
-  assert_select 'div.field_with_errors'#. (the period or dot) targest MULTIPLE elements within a class
-  #assert_select 'div id="error_explanation"' My wrong syntax attempt at testing error messages of
-  #CSS id for error explanation and CSS class for field with error
+  assert_template 'users/new' #Test it re-renders new template from users controller.
+
+  #Use assert_select to test HTML elements of the unlikely-to-change pages
+  assert_select 'div#error_explanation'
+  #The hash symbol in CSS targets a single specific element with unique ID, the CSS ID for error_explanation.
+  assert_select 'div.field_with_errors'
+  #The CSS dot "." symbol targest multiple elements within a class, after the dot is the class for field with error
+  assert_select 'div.alert'
 
   end
-  #^^^before_count = User.count
-  #post users_path, ...
-  #after_count  = User.count
-  #assert_equal before_count, after_count
 
+  #Test successful submission that actually creates a new user
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -37,6 +42,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
-assert_not flash.empty? #can delete this line? I think not
+    assert_not flash.empty? #can delete this line? I think not
   end
 end
