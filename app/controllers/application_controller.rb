7bf30c5c36a60eb83fc1dbb3d1d8a4ac965e62cc
upgradeseason <junglerainforest@gmail.com>
2021-10-17 20:45:44 -0700

    Fix production flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
   #protect_from_forgery prepend: true
-  #protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 end
