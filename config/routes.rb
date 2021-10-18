15d7e6b0893206d12d0ad67777c43a790376b75b
upgradeseason <junglerainforest@gmail.com>
2021-10-18 12:15:54 -0700

    Fix comment syntax

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,18 @@ Rails.application.routes.draw do
   #Mapping establishes that root URL will be static pgs controller, home action.
   root 'static_pages#home'
 
-  #get 'sessions/new' #only uses named routes(GET and POST handled with #login route, DELETE with #logout route.
-  #get 'users/new'
+  #Sessions controller only uses named routes
   #Note that the Signup page is routed to the NEW action in users controller
 
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
   get    '/signup',  to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  #Post request is handled by create action"
+  #GET (sessions#new) is handled with login route/path/URL to sessions controller
+  #These are the 3 routes for the sessions form.
+  get    '/login',   to: 'sessions#new' #The login page
+  #POST request is handled by/routed to (sessions controller) create action
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  delete '/logout',  to: 'sessions#destroy' #Destroy an object/session
   resources :users #special resources method used to obtain full suite of RESTful routes automatically
 end
