2131ba4f87346dcb54b4b98825bad24eb641e00c
upgradeseason <junglerainforest@gmail.com>
2021-10-21 14:26:55 -0700

    Fix production js bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   #protect_from_forgery prepend: true
   #protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token #First fix attempt
 end
 
 #This is base class of all the controllers
