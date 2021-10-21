3741b32aabd384759956ede87d694af15dba05c4
upgradeseason <junglerainforest@gmail.com>
2021-10-21 14:21:53 -0700

    Fix js menu bug on production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
-  protect_from_forgery prepend: true
+  #protect_from_forgery prepend: true
   #protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
 end
 
 #This is base class of all the controllers
