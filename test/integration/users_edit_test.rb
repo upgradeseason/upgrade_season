09af5a53cedf19e84b437c41de21b7076a75fd1c
upgradeseason <junglerainforest@gmail.com>
2021-10-22 21:42:40 -0700

    Add edit user tests

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -17,11 +17,24 @@ class UsersEditTest < ActionDispatch::IntegrationTest
       #PATCH request to users_path
       #params[:user] hash is expected by the User.new create action.
       #We're simulating form submission here.
-                                    email: "user@invalid",
-                                    password:              "foo",
-                                    password_confirmation: "bar" }}
+                                              email: "user@invalid",
+                                              password:              "foo",
+                                              password_confirmation: "bar" }}
     #Test right template is rendered
     assert_template 'users/edit'
     #assert_select 'div/alert', text: "The form contains 4 errors."
+  end
+
+  test "valid edit" do
+    get edit_user_path(@user)
+    patch user_path(@user), params: { user: { name: "Foo Bar",
+                                              email: "foo@bar.com",
+                                              password:              "",
+                                              password_confirmation: "" } }
+  assert_not flash.empty? #Dont need to test actual key
+  assert_redirected_to @user
+  @user.reload
+  assert_equal @user.name, "Foo Bar"
+  assert_equal @user.email, "foo@bar.com"
   end
 end
