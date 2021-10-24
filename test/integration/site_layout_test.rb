fb261fca8399b4f31d63c7a266ffbcdd6706febf
upgradeseason <junglerainforest@gmail.com>
2021-10-24 14:05:37 -0700

    Add tests for new layout

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -17,5 +17,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign Up")
+    assert_select "a[href=?]", login_path
+    user = users(:danelli)
+    log_in_as(user)
+    get root_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(user)
+    assert_select "a[href=?]", edit_user_path(user)
   end
 end
