d678df461c7c3c478c5a705570535a423dec24a3
upgradeseason <junglerainforest@gmail.com>
2021-10-31 15:00:12 -0700

    Add frontend for following users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 #Define URLs and where they go to
 
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
-  get 'account_activations/edit'
+  #get 'password_resets/new'
+  # We dont need this, it came from generating things
+  #get 'password_resets/edit'
+  #get 'account_activations/edit'
   #Mapping establishes that root URL will be static pgs controller, home action.
   root 'static_pages#home'
 
@@ -20,10 +21,22 @@ Rails.application.routes.draw do
   #POST request is handled by/routed to (sessions controller) create action
   post   '/login',   to: 'sessions#create' #And CREATE to complete the login
   delete '/logout',  to: 'sessions#destroy' #Destroy an object/session
+  resources :users do
+    # Give a block to resources :users, say member do, responding to GET requests
+    # We make a route to give us that kind of URL
+    member do
+      get :following, :followers
+    end
+    # If didn't want ID in URL, EG users/tigers vs users/1/following
+    #collection do
+    #  get :tigers
+    #end
+  end
   resources :users #special resources method used to obtain full suite of RESTful routes automatically
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   #Add some routes for this, creating and destroying microposts, for mp resource
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   #Break in convention: we didn't add :new or :edit (in mp ctrlr) bc we're manipulating mp's thru user home and profile page, rather than separate views
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rails db:seed command.
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#Bang raises exception if it doesn't work, it's a noisy method => avoids silent errors, makes debugging easier
+# For users
+# Bang raises exception if it doesn't work, it's a noisy method => avoids silent errors, makes debugging easier
 User.create!(name:  "Example User",
              email: "example1234@upgradeseason.com",
              password:              "foobar",
@@ -28,9 +29,20 @@ User.create!(name:  "Example User",
 
 end
 
+# For microposts
 #users = User.order(:created_at).take(6)
 users = User.order(:created_at).first(6)
 50.times do #Doesn't create big runs
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Add some code for creating following relationships (enough to create interface)
+users = User.all #Select all users in DB
+user = User.first #Pick out first one
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) } #Call user.follow for each of these users to be followed
+# User follows users 3..49
+followers.each { |follower| follower.follow(user) } #Each follower should follow the user.
+# Users 4 through 39 follow that user back
